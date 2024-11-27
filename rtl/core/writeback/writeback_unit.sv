9b45f5d8fec90d58466c1f68684b7c58f8c5c8da
Raghavendra Davarapalli <ragha@mo.ece.pdx.edu>
2024-11-27 02:01:27 -0800

    updated writeback stage to be compatible with top module

--- rtl/core/writeback/writeback_unit.sv
+++ rtl/core/writeback/writeback_unit.sv
@@ -1,14 +1,42 @@
+///////////////////////////////////////////////////////////////////////
+// Module: Writeback Stage
+//
+// Description:
+// This module implements the writeback stage of the RISC-V processor. 
+// It is responsible for selecting the appropriate data (either ALU result 
+// or Load Memory Data) and writing it back to the register file. The 
+// writeback operation occurs only if the `write_en` signal is enabled and 
+// the destination register address is valid (non-zero).
+//
+// Interfaces: register_file_if, memory_writeback_if
+//
+//
+// Functionality:
+// - For load instructions (`OPCODE_LOAD`), the `LMD` is written back.
+// - For non-load instructions, the `alu_result` is written back.
+// - If `write_en` is enabled and `rd_addr` is non-zero, the computed 
+//   writeback data is assigned to `rd_data`.
+//
+///////////////////////////////////////////////////////////////////////
 module writeback_stage (
     register_file_if.writeback_reg rf_write_if,  // Register file interface for writeback stage
-    memory_writeback_if.writeback_stage mw_if // Memory to Writeback stage interface
+    memory_writeback_if.writeback_stage mw_if // Memory to Writeback stage interface  
 );
-  import riscv_pkg::*;
-
+    import riscv_pkg::*;
+   
     // Select data for writeback
     logic [XLEN-1:0] wb_data;
-    assign wb_data = mw_if.mem_to_reg ? mw_if.LMD : mw_if.address; //mem_to_reg is high for load instructions, low for arithmetic or logical operations
+  always_comb begin
+    if(mw_if.opcode == OPCODE_LOAD)
+      wb_data = mw_if.LMD;
+    else
+      wb_data = mw_if.alu_result;
+  end
+  
+    assign rf_write_if.rd_addr = mw_if.decoded_instr.rd;
 
     // Directly assign rd_data based on write enable and address validity
     assign rf_write_if.rd_data = (rf_write_if.write_en && rf_write_if.rd_addr != '0) ? wb_data : '0;
 
 endmodule : writeback_stage
+
